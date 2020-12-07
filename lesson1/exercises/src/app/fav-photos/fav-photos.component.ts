6d4a2a57d12edd8a696bf1cf024f81f10e0e9f3a
Gerardo Delgado <gdelgado100@gmail.com>
2020-12-07 13:04:17 -0600

    Part 1 of exercise complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Inspiration';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://kids.kiddle.co/images/1/19/Batman_%28circa_2016%29.png';
+  image3 = 'https://img.cinemablend.com/filter:scale/quill/d/2/f/8/6/b/d2f86bf4f8b02b0f542a1f3ffd2efabe09188f4c.jpg?mw=600';
 
   constructor() { }
 
